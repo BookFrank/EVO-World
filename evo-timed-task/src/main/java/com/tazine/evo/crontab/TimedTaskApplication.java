171f5ec595c112d4d9d592b08f0acba0a2085103
jiaer.ly <jiaer.ly@alibaba-inc.com>
2018-09-06 17:45:29 +0800

    feat (evo-timed-task) : combine @Async with @Scheduled.

--- evo-timed-task/src/main/java/com/tazine/evo/crontab/TimedTaskApplication.java
+++ evo-timed-task/src/main/java/com/tazine/evo/crontab/TimedTaskApplication.java
@@ -2,6 +2,7 @@ package com.tazine.evo.crontab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * 应用启动类
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author frank
  * @date 2018/09/05
  */
+@EnableAsync
 @SpringBootApplication
 public class TimedTaskApplication {
 
--- evo-timed-task/src/main/java/com/tazine/evo/crontab/spring/SimpleSpringTask.java
+++ evo-timed-task/src/main/java/com/tazine/evo/crontab/spring/SimpleSpringTask.java
@@ -1,6 +1,7 @@
 package com.tazine.evo.crontab.spring;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -14,8 +15,30 @@ import org.springframework.scheduling.annotation.Scheduled;
 @EnableScheduling
 public class SimpleSpringTask {
 
+
     @Scheduled(cron = "0/5 * * * * ?")
-    public void schedule() {
-        //System.out.println("Hello World");
+    public void schedule1() {
+        System.out.println("Hello World -- " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void schedule2() {
+        System.out.println("Brave World -- " + Thread.currentThread().getName());
+    }
+
+    @Async
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void schedule3() {
+        System.out.println("Mad World -- " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
