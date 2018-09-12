0002e502e14ff0b1d0dbb39d36e895009fe031b2
Frank <bookfrank@foxmail.com>
2018-09-12 22:45:16 +0800

    feat (elastic-search-6) : 增加 ES 6 实践模块。

---
+++ evo-elastic-search-6/src/main/java/com/tazine/evo/elastic/EvoElasticSearch6Application.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoElasticSearch6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoElasticSearch6Application.class, args);
+	}
+}
