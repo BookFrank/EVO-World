f9b5dd7ee109f04de5fd3f46bbd67a5be428fdf4
Frank <bookfrank@foxmail.com>
2018-09-13 23:41:34 +0800

    feat (springboot-mongo) : 增加 SpringBoot 与 Mongo 整合实践。

---
+++ evo-springboot-mongo/src/main/java/com/tazine/evo/mongo/EvoSpringbootMongoApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoSpringbootMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoSpringbootMongoApplication.class, args);
+	}
+}
---
+++ evo-springboot-mongo/src/test/java/com/tazine/evo/mongo/EvoSpringbootMongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tazine.evo.mongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EvoSpringbootMongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
