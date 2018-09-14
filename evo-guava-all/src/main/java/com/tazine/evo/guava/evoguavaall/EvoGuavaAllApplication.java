ccb96fda1608a22cd9400b4c0c373d9d15628f23
Frank <bookfrank@foxmail.com>
2018-09-14 23:55:51 +0800

    feat (guava) : 增加 Guava 实践相关模块。

---
+++ evo-guava-all/src/main/java/com/tazine/evo/guava/evoguavaall/EvoGuavaAllApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.guava.evoguavaall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoGuavaAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoGuavaAllApplication.class, args);
+	}
+}
