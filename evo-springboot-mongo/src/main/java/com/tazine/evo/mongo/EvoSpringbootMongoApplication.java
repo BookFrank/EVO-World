347da387bf5a4ed9a3852839075b5102ddb06a28
Frank <bookfrank@foxmail.com>
2018-09-14 23:51:45 +0800

    feat (redis-mongo) : redis 和 mongo 部分代码格式化。

--- evo-springboot-mongo/src/main/java/com/tazine/evo/mongo/EvoSpringbootMongoApplication.java
+++ evo-springboot-mongo/src/main/java/com/tazine/evo/mongo/EvoSpringbootMongoApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EvoSpringbootMongoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EvoSpringbootMongoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EvoSpringbootMongoApplication.class, args);
+    }
 }
+
