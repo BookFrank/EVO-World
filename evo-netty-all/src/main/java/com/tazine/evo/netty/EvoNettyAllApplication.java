97e384da1ba05cc5f4e068258288255c5034669a
Frank <bookfrank@foxmail.com>
2018-09-15 21:34:44 +0800

    feat (netty-all) : 增加 Netty 实践模块。

---
+++ evo-netty-all/src/main/java/com/tazine/evo/netty/EvoNettyAllApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoNettyAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoNettyAllApplication.class, args);
+	}
+}
