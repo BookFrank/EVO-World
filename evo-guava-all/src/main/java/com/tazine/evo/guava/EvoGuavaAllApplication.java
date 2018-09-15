a5e0965b5c7d2bccf1c1e00996359c3041cc5595
Frank <bookfrank@foxmail.com>
2018-09-15 23:24:15 +0800

    style (guava-all) : Guava web启动类增加代码注释。

--- evo-guava-all/src/main/java/com/tazine/evo/guava/EvoGuavaAllApplication.java
+++ evo-guava-all/src/main/java/com/tazine/evo/guava/EvoGuavaAllApplication.java
@@ -3,10 +3,16 @@ package com.tazine.evo.guava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Guava 实践框架相关启动类
+ *
+ * @author frank
+ * @date 2018/09/15
+ */
 @SpringBootApplication
 public class EvoGuavaAllApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EvoGuavaAllApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EvoGuavaAllApplication.class, args);
+    }
 }
