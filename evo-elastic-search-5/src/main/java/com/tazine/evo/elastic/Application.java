cb37410eddebe29cac0ba5ab4526564621493a4f
Frank <bookfrank@foxmail.com>
2018-09-16 20:08:42 +0800

    feat (elastic-search-5) : es5 实践启动类增加注释。

--- evo-elastic-search-5/src/main/java/com/tazine/evo/elastic/Application.java
+++ evo-elastic-search-5/src/main/java/com/tazine/evo/elastic/Application.java
@@ -3,10 +3,16 @@ package com.tazine.evo.elastic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringBoot 与 Elastic5 整合实践启动类
+ *
+ * @author frank
+ * @date 2018/09/15
+ */
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
