aff21edf7a1448ad54623aa4b72f3bb56d5eafd3
Frank <bookfrank@foxmail.com>
2018-09-16 20:10:04 +0800

    feat (springboot-mongo) : mongo 实践启动类增加注释。

--- evo-springboot-mongo/src/main/java/com/tazine/evo/mongo/EvoSpringbootMongoApplication.java
+++ evo-springboot-mongo/src/main/java/com/tazine/evo/mongo/EvoSpringbootMongoApplication.java
@@ -3,6 +3,12 @@ package com.tazine.evo.mongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringBoot 与 Mongo 整合实践启动类
+ *
+ * @author frank
+ * @date 2018/09/15
+ */
 @SpringBootApplication
 public class EvoSpringbootMongoApplication {
 
