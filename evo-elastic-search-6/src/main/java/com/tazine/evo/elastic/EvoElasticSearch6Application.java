1b2dc1bd68f06271f4ff4be5983e91308bb693f9
Frank <bookfrank@foxmail.com>
2018-09-16 20:09:02 +0800

    feat (elastic-search-6) : es6 实践启动类增加注释。

--- evo-elastic-search-6/src/main/java/com/tazine/evo/elastic/EvoElasticSearch6Application.java
+++ evo-elastic-search-6/src/main/java/com/tazine/evo/elastic/EvoElasticSearch6Application.java
@@ -3,6 +3,12 @@ package com.tazine.evo.elastic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringBoot 与 Elastic6 整合实践启动类
+ *
+ * @author frank
+ * @date 2018/09/15
+ */
 @SpringBootApplication
 public class EvoElasticSearch6Application {
 
