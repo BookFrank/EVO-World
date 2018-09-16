3436a0247da76bbc2b2491b3db1d9b3ee9c4aaee
Frank <bookfrank@foxmail.com>
2018-09-16 20:06:22 +0800

    feat (springboot-redis) : 修改 redis 实践启动类并增加注释。

---
+++ evo-springboot-redis/src/main/java/com/tazine/evo/redis/EvoSpringBootRedisApplication.java
@@ -0,0 +1,18 @@
+package com.tazine.evo.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot 与 Redis 整合实践启动类
+ *
+ * @author frank
+ * @date 2018/09/15
+ */
+@SpringBootApplication
+public class EvoSpringBootRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvoSpringBootRedisApplication.class, args);
+    }
+}
--- evo-springboot-redis/src/test/java/com/tazine/evo/redis/EvoSpringBootRedisApplicationTests.java
+++ evo-springboot-redis/src/test/java/com/tazine/evo/redis/EvoSpringBootRedisApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EvoSpringbootRedisApplicationTests {
+public class EvoSpringBootRedisApplicationTests {
 
 	@Test
 	public void contextLoads() {
