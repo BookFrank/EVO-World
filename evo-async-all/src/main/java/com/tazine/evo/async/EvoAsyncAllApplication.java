b367176133577dafe44e9b1da6b31ec23df77cad
Frank <bookfrank@foxmail.com>
2018-09-16 20:06:58 +0800

    feat (async-all) : Java 中异步的各种实现以及调优实践。

---
+++ evo-async-all/src/main/java/com/tazine/evo/async/EvoAsyncAllApplication.java
@@ -0,0 +1,18 @@
+package com.tazine.evo.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 异步各种实践，异步 SpringBoot 启动类
+ *
+ * @author frank
+ * @date 2018/09/15
+ */
+@SpringBootApplication
+public class EvoAsyncAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvoAsyncAllApplication.class, args);
+    }
+}
---
+++ evo-async-all/src/test/java/com/tazine/evo/async/EvoAsyncAllApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tazine.evo.async;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EvoAsyncAllApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
