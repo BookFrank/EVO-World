36a582a51cfc1becd4718da3d7795dc40dfc6ae4
Frank <bookfrank@foxmail.com>
2018-09-17 22:38:46 +0800

    feat (elastic-search-5) : ES 配置类，实现 TCP TransportClient bean 的配置以及连接工作。

---
+++ evo-elastic-search-5/src/main/java/com/tazine/evo/elastic/client/config/ElasticSearchConfig.java
@@ -0,0 +1,102 @@
+package com.tazine.evo.elastic.client.config;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * ES 配置类
+ *
+ * @author frank
+ * @date 2018/08/10
+ */
+@Configuration
+public class ElasticSearchConfig implements FactoryBean<TransportClient>, InitializingBean, DisposableBean {
+
+    private static final Logger logger = LoggerFactory.getLogger(ElasticSearchConfig.class);
+
+    /**
+     * 名称
+     */
+    @Value("${spring.data.elasticsearch.cluster-name}")
+    private String clusterName;
+
+    /**
+     * 节点
+     */
+    @Value("${spring.data.elasticsearch.cluster-nodes}")
+    private String clusterNodes;
+
+    private TransportClient transportClient;
+
+    @Override
+    public TransportClient getObject() throws Exception {
+        return transportClient;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return TransportClient.class;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        buildClient();
+    }
+
+    protected void buildClient() {
+        System.out.println(clusterName + clusterNodes);
+        try {
+            PreBuiltXPackTransportClient preBuiltXPackTransportClient = new PreBuiltXPackTransportClient(settings());
+            if (!"".equals(clusterNodes)) {
+                for (String nodes : clusterNodes.split(",")) {
+                    String inetSocket[] = nodes.split(":");
+                    String address = inetSocket[0];
+                    Integer port = Integer.valueOf(inetSocket[1]);
+                    preBuiltXPackTransportClient.addTransportAddress(new
+                        InetSocketTransportAddress(InetAddress.getByName(address), port));
+                }
+                transportClient = preBuiltXPackTransportClient;
+            }
+        } catch (UnknownHostException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    /**
+     * 初始化默认的client
+     */
+    private Settings settings() {
+        Settings settings = Settings.builder()
+            .put("cluster.name", clusterName)
+            .put("xpack.security.transport.ssl.enabled", false)
+            .put("xpack.security.user", "elastic:changeme")
+            // 设置client.transport.sniff为true来使客户端去嗅探整个集群的状态，把集群中其它机器的ip地址加到客户端中，这样做的好处是一般你不用手动设置集群里所有集群的ip到连接客户端，它会自动帮你添加，并且自动发现新加入集群的机器
+            .put("client.transport.sniff", false)
+            .build();
+        //transportClient = new PreBuiltXPackTransportClient(settings);
+        return settings;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        try {
+            if (transportClient != null) {
+                transportClient.close();
+            }
+        } catch (final Exception e) {
+            logger.error("Error closing ElasticSearch client: ", e);
+        }
+    }
+}
