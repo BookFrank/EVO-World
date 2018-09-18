68efb42c65fc37941b5c01e5ee0a3f3bca749cae
Frank <bookfrank@foxmail.com>
2018-09-18 23:27:06 +0800

    feat (httpclient-all) : 增加 httpclient 实践模块。

---
+++ evo-httpclient-all/src/main/java/com/tazine/evo/http/EvoHttpclientAllApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoHttpclientAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoHttpclientAllApplication.class, args);
+	}
+}
