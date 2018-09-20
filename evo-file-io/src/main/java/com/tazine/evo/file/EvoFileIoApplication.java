1f703bae3c5fb9439a23fcf7b9356a0f93c40184
Frank <bookfrank@foxmail.com>
2018-09-20 23:36:08 +0800

    feat (file-io) : 启动类增加注释。

--- evo-file-io/src/main/java/com/tazine/evo/file/EvoFileIoApplication.java
+++ evo-file-io/src/main/java/com/tazine/evo/file/EvoFileIoApplication.java
@@ -3,10 +3,16 @@ package com.tazine.evo.file;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 文件读写总结启动类
+ *
+ * @author frank
+ * @date 2018/09/20
+ */
 @SpringBootApplication
 public class EvoFileIoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EvoFileIoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EvoFileIoApplication.class, args);
+    }
 }
