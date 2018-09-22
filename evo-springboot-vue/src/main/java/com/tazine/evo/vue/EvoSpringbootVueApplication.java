f17117aaffc9a53ea5121948ffd14951de45adcc
Frank <bookfrank@foxmail.com>
2018-09-22 22:58:09 +0800

    feat (springboot-vue) : 增加 SpringBoot Vue SPA 整合学习模块

---
+++ evo-springboot-vue/src/main/java/com/tazine/evo/vue/EvoSpringbootVueApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoSpringbootVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoSpringbootVueApplication.class, args);
+	}
+}
