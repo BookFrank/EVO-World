e79c352f694ee86d8cc26cd743c4aa312c4e93ff
Frank <bookfrank@foxmail.com>
2018-09-24 23:12:45 +0800

    feat (springboot-dubbo) : 增加 springboot-dubbo 学习。

---
+++ evo-springboot-dubbo/src/main/java/com/tazine/evo/dubbo/evospringbootdubbo/EvoSpringbootDubboApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.dubbo.evospringbootdubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoSpringbootDubboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoSpringbootDubboApplication.class, args);
+	}
+}
---
+++ evo-springboot-dubbo/src/test/java/com/tazine/evo/dubbo/evospringbootdubbo/EvoSpringbootDubboApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tazine.evo.dubbo.evospringbootdubbo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EvoSpringbootDubboApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
