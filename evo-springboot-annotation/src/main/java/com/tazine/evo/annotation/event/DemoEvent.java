ae1c59e70ce81333251e69a33161f4fc3170b294
Frank <bookfrank@foxmail.com>
2018-09-24 23:08:47 +0800

    feat (springboot-annotation) : 事件注释修改。

--- evo-springboot-annotation/src/main/java/com/tazine/evo/annotation/event/DemoEvent.java
+++ evo-springboot-annotation/src/main/java/com/tazine/evo/annotation/event/DemoEvent.java
@@ -3,11 +3,12 @@ package com.tazine.evo.annotation.event;
 import org.springframework.context.ApplicationEvent;
 
 /**
- * @author jiaer.ly
+ * Demo 事件
+ *
+ * @author frank
  * @date 2018/09/23
  */
 public class DemoEvent extends ApplicationEvent {
-    
 
     public DemoEvent(Object source) {
         super(source);
