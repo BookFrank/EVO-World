3c034ef12d6cef92b94b09e1b0c3a7d621ff6a29
Frank <bookfrank@foxmail.com>
2018-09-25 20:48:10 +0800

    feat (springboot-dubbo) : dubbo 整合启动类修改与注释。

---
+++ evo-springboot-dubbo/src/main/java/com/tazine/evo/dubbo/EvoSpringBootDubboApplication.java
@@ -0,0 +1,18 @@
+package com.tazine.evo.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot 与dubbo整合学习启动类
+ *
+ * @author frank
+ * @date 2018/09/22
+ */
+@SpringBootApplication
+public class EvoSpringBootDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvoSpringBootDubboApplication.class, args);
+    }
+}
--- evo-springboot-dubbo/src/test/java/com/tazine/evo/dubbo/EvoSpringBootDubboApplicationTests.java
+++ evo-springboot-dubbo/src/test/java/com/tazine/evo/dubbo/EvoSpringBootDubboApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EvoSpringbootDubboApplicationTests {
+public class EvoSpringBootDubboApplicationTests {
 
 	@Test
 	public void contextLoads() {
