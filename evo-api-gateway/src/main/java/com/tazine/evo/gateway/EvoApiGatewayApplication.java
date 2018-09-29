85019cba21aecc51df3f2bbad7c58c721190c457
Frank <bookfrank@foxmail.com>
2018-09-29 23:42:41 +0800

    feat (api-gateway) : 增加 API 网关学习。

---
+++ evo-api-gateway/src/main/java/com/tazine/evo/gateway/EvoApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoApiGatewayApplication.class, args);
+	}
+}
