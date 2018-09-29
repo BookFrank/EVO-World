485caa6f07b2f7ad8d93e5e11c0442b794a60332
Frank <bookfrank@foxmail.com>
2018-09-29 23:46:24 +0800

    feat (api-gateway) : 增加api网关启动类注释。

--- evo-api-gateway/src/main/java/com/tazine/evo/gateway/EvoApiGatewayApplication.java
+++ evo-api-gateway/src/main/java/com/tazine/evo/gateway/EvoApiGatewayApplication.java
@@ -3,6 +3,12 @@ package com.tazine.evo.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * API 网关 SpringBoot 启动类
+ *
+ * @author frank
+ * @date 2018/09/15
+ */
 @SpringBootApplication
 public class EvoApiGatewayApplication {
 
