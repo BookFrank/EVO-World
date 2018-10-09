e67b9bf71d397d46d30eb74480d738e21b3db351
Frank <bookfrank@foxmail.com>
2018-10-09 11:42:20 +0800

    feat (unit-test) : 增加单元测试学习实践模块。

---
+++ evo-unit-test/src/main/java/com/tazine/evo/unit/EvoUnitTestApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.unit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoUnitTestApplication.class, args);
+	}
+}
