59b73640b24f6c2b218f32cd7c709da4bcc8b2f4
Frank <bookfrank@foxmail.com>
2018-10-16 23:32:48 +0800

    feat (file-io) : 增加文件IO测试。

--- evo-file-io/src/main/java/com/tazine/evo/file/FileReader.java
+++ evo-file-io/src/main/java/com/tazine/evo/file/FileReader.java
@@ -8,17 +8,49 @@ import java.net.URL;
 import java.util.List;
 
 /**
- *
  * @author frank
  * @date 2018/10/11
  */
 public class FileReader {
 
     public static void main(String[] args) {
+        readTest();
+        //resourceRead();
+    }
+
+    private static void readTest() {
+        System.out.println(FileReader.class.getResource("a.txt"));
+        System.out.println(FileReader.class.getResource("/a.txt"));
+        System.out.println(FileReader.class.getResource("/"));
+        System.out.println(FileReader.class.getResource("b.txt"));
+        System.out.println();
+
+        // need to add resource/b.txt to build path! it will be package in jar file
+        System.out.println(FileReader.class.getClassLoader().getResource("a.txt"));
+        System.out.println(FileReader.class.getClassLoader().getResource("/a.txt"));
+        System.out.println(FileReader.class.getClassLoader().getResource("/"));
+        System.out.println(Thread.currentThread().getContextClassLoader().getResource("a.txt"));
+    }
+
+    /**
+     * Resources.read
+     */
+    private static void resourceRead() {
         URL url = FileReader.class.getClassLoader().getResource("area.csv");
         try {
             // Commons-io 读取文件
-            List<String> list =  FileUtils.readLines(new File(url.getFile()), "UTF-8");
+            List<String> list = FileUtils.readLines(new File(url.getFile()), "UTF-8");
+            list.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void classLoaderResourceRead() {
+        URL url = FileReader.class.getClassLoader().getResource("area.csv");
+        try {
+            // Commons-io 读取文件
+            List<String> list = FileUtils.readLines(new File(url.getFile()), "UTF-8");
             list.forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
