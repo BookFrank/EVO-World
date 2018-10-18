2dac6dd50d48764c8eb94d05686a6fd8354e691e
Frank <bookfrank@foxmail.com>
2018-10-18 16:53:40 +0800

    feat (neo4j) : Neo4j 图数据的使用。

---
+++ evo-httpclient-all/src/main/java/com/tazine/evo/http/util/Test.java
@@ -0,0 +1,8 @@
+package com.tazine.evo.http.util;
+
+/**
+ * @author jiaer.ly
+ * @date 2018/10/17
+ */
+public class Test {
+}
---
+++ evo-neo4j/src/main/java/com/tazine/evo/evoneo4j/EvoNeo4jApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.evoneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoNeo4jApplication.class, args);
+	}
+}
