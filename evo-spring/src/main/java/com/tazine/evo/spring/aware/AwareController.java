4afbcd61335f48fa74e1251d760edc8f8cfcdec2
liyang <liyang12@yiche.com>
2018-10-31 23:56:49 +0800

    feat (spring) : 增加 Spring Aware 相关实践。

---
+++ evo-spring/src/main/java/com/tazine/evo/spring/aware/AwareController.java
@@ -0,0 +1,43 @@
+package com.tazine.evo.spring.aware;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring 中的 Aware 感知
+ *
+ * @author frank
+ * @date 2018/10/17
+ */
+@RestController
+public class AwareController {
+
+    @Autowired
+    private ResourceAwareService resourceAwareService;
+
+    @Autowired
+    private ContextAwareService contextAwareService;
+
+    @Autowired
+    private ApplicationContext context;
+
+    @RequestMapping("/res/aware")
+    public void resourceAware() {
+        resourceAwareService.outputResult();
+    }
+
+    @RequestMapping("/ctx/aware")
+    public void contextAware() {
+        contextAwareService.output();
+    }
+
+    @RequestMapping("/ctx")
+    public String ctx() {
+        System.out.println(JSON.toJSONString(context.getEnvironment()));
+        String name = context.getEnvironment().getProperty("spring.application.name");
+        return name;
+    }
+}
---
+++ evo-spring/src/main/java/com/tazine/evo/spring/aware/ResourceAwareService.java
@@ -0,0 +1,45 @@
+package com.tazine.evo.spring.aware;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+/**
+ * AwareService，实现 BeanNameAware 和 ResourceLoaderAware 接口，获得 Bean 名称和资源加载的服务
+ *
+ * @author frank
+ * @date 2018/09/26
+ */
+@Service
+public class ResourceAwareService implements BeanNameAware, ResourceLoaderAware {
+
+    private String beanName;
+    private ResourceLoader loader;
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.loader = resourceLoader;
+    }
+
+    public void outputResult() {
+        System.out.println("Bean 的名称为：" + beanName);
+        // 如果文件在 Resources 目录下，直接 classpath:文件名 即可
+        Resource resource = loader.getResource("classpath:aware.txt");
+        try {
+            System.out.println("ResourceLoader 加载的文件内容为：" + IOUtils.toString(resource.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
