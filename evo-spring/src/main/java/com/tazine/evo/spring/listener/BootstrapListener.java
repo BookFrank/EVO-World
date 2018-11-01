4d59ee45ffea7af05711e47957461a776a088713
Frank <bookfrank@foxmail.com>
2018-11-01 17:08:46 +0800

    feat (spring) : SpringBoot CommandLineRunner 初始化。

--- evo-spring/src/main/java/com/tazine/evo/spring/listener/BootstrapListener.java
+++ evo-spring/src/main/java/com/tazine/evo/spring/listener/BootstrapListener.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 public class BootstrapListener implements ApplicationListener<ContextRefreshedEvent> {
 
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        System.out.println("Listener 统计到当前 Bean 个数为：" + contextRefreshedEvent.getApplicationContext().getBeanDefinitionCount());
+        System.err.println("Listener 统计到当前 Bean 个数为：" + contextRefreshedEvent.getApplicationContext().getBeanDefinitionCount());
 
         // SpringMvc 中需要判断，防止启动两次，SpringBoot 不会传二次调用问题
         //if (contextRefreshedEvent.getApplicationContext().getParent() != null){
