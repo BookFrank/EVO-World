a98955c4007158da58e09806fd34bd256a717a48
Frank <bookfrank@foxmail.com>
2018-11-04 23:05:17 +0800

    feat (netty) : Netty 时间服务器整理。

---
+++ evo-netty-all/src/main/java/com/tazine/evo/socket/netty/TimeServer.java
@@ -0,0 +1,46 @@
+package com.tazine.evo.socket.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 基于 Netty 的 NIO 时间服务器
+ *
+ * @author frank
+ * @date 2017/10/31
+ */
+public class TimeServer {
+
+    public void bind(int port){
+
+        // 服务端为什么需要线程组？
+        // 配置服务端的 NIO 线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new EvoChannelHandler());
+
+            // 绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+
+            // 等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+}
