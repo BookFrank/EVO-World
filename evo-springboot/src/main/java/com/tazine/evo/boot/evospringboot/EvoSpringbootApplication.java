555b5f8c8210113a18a2c7594993f78404682f75
Frank <bookfrank@foxmail.com>
2018-11-05 14:29:41 +0800

    feat (springboot) : 增加 SpringBoot 模块。

---
+++ evo-springboot/src/main/java/com/tazine/evo/boot/evospringboot/EvoSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.boot.evospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoSpringbootApplication.class, args);
+	}
+}
