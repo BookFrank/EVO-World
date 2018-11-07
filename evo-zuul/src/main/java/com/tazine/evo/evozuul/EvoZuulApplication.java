497df0a61ceaad587fe7f5259f9eeb5545146775
Frank <bookfrank@foxmail.com>
2018-11-07 21:11:33 +0800

    feat (zuul) : 增加 zuul 模块。

---
+++ evo-zuul/src/main/java/com/tazine/evo/evozuul/EvoZuulApplication.java
@@ -0,0 +1,12 @@
+package com.tazine.evo.evozuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoZuulApplication.class, args);
+	}
+}
