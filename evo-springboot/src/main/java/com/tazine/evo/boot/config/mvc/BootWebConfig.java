a43c6dc345c3e5974e9a8bed907cb73cff77bb4a
Frank <bookfrank@foxmail.com>
2018-11-13 15:39:28 +0800

    feat (springboot) : 增加 date 参数接收实践。

--- evo-springboot/src/main/java/com/tazine/evo/boot/config/mvc/BootWebConfig.java
+++ evo-springboot/src/main/java/com/tazine/evo/boot/config/mvc/BootWebConfig.java
@@ -1,15 +1,21 @@
-package com.tazine.evo.boot.interceptor;
+package com.tazine.evo.boot.config.mvc;
 
+import com.tazine.evo.boot.interceptor.EvoBootInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 /**
- * @author jiaer.ly
+ * 配置方法一: SpringMvcConfig 配置
+ *
+ * @author frank
  * @date 2018/11/07
  */
-@Configuration
+//@Configuration
 public class BootWebConfig implements WebMvcConfigurer {
 
     private final EvoBootInterceptor evoBootInterceptor;
@@ -22,4 +28,5 @@ public class BootWebConfig implements WebMvcConfigurer {
         registry.addInterceptor(evoBootInterceptor)
             .addPathPatterns("/*");
     }
+
 }
