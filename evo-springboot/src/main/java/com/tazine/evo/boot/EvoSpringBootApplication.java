8ed97f002822eabfa6edecf461289b918893fc9b
Frank <bookfrank@foxmail.com>
2018-11-13 14:45:49 +0800

    feat (springboot) : 启动类名称修改。

--- evo-springboot/src/main/java/com/tazine/evo/boot/EvoSpringBootApplication.java
+++ evo-springboot/src/main/java/com/tazine/evo/boot/EvoSpringBootApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * SpringBoot2.0 实践
+ *
+ * @author frank
+ * @date 2018/11/11
+ */
 @ServletComponentScan
 @SpringBootApplication
-public class EvoSpringbootApplication {
+public class EvoSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EvoSpringbootApplication.class, args);
+		SpringApplication.run(EvoSpringBootApplication.class, args);
 	}
 }
