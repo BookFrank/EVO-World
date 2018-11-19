34579f0248f22de0bea6fbfed8e4bb80c34b5b53
Frank <bookfrank@foxmail.com>
2018-11-19 23:52:45 +0800

    feat (non-web) : 增加启动类。

---
+++ evo-springboot-nonweb/src/main/java/com/tazine/evo/non/Application.java
@@ -0,0 +1,24 @@
+package com.tazine.evo.non;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用入口方法
+ *
+ * @author jiaer.ly
+ * @date 2018/11/19
+ */
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
