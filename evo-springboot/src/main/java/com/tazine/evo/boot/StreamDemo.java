a1bb588096b4c28bb65995bc8cac75c92ecb631f
Frank <bookfrank@foxmail.com>
2018-11-20 23:22:53 +0800

    feat (springboot) : Java8 stream demo.

---
+++ evo-springboot/src/main/java/com/tazine/evo/boot/StreamDemo.java
@@ -0,0 +1,17 @@
+package com.tazine.evo.boot;
+
+/**
+ * Java8 Stream
+ *
+ * @author frank
+ * @date 2018/11/20
+ */
+public class StreamDemo {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
