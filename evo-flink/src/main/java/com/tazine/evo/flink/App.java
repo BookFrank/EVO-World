6c37c5c49823c029026cb2cfc9fecea338c5f19a
Frank <bookfrank@foxmail.com>
2018-11-23 11:42:36 +0800

    feat (flink) : 增加 flink 开发模块。

---
+++ evo-flink/src/main/java/com/tazine/evo/flink/App.java
@@ -0,0 +1,15 @@
+package com.tazine.evo.flink;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        String s = " to be or not to be";
+
+        //for (String word : )
+
+        System.out.println("Hello World!");
+    }
+}
