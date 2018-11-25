3ae5b23308592ce2333ddc5786a3c87ff43cac8d
Frank <bookfrank@foxmail.com>
2018-11-25 22:19:44 +0800

    feat (unit-test) : 单元测试。

---
+++ evo-unit-test/src/main/java/com/tazine/evo/unit/service/HelloService.java
@@ -0,0 +1,18 @@
+package com.tazine.evo.unit.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * HelloService
+ *
+ * @author frank
+ * @date 2018/11/25
+ */
+@Service
+public class HelloService {
+
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
+
+}
