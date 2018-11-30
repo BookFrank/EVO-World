23231af32b3db10f566973a656e8ddf199197773
Frank <bookfrank@foxmail.com>
2018-11-30 23:08:03 +0800

    feat (unit-test) : 单元测试。

--- evo-unit-test/src/main/java/com/tazine/evo/unit/EvoUnitTestApplication.java
+++ evo-unit-test/src/main/java/com/tazine/evo/unit/EvoUnitTestApplication.java
@@ -3,6 +3,12 @@ package com.tazine.evo.unit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 单元测试启动类
+ *
+ * @author frank
+ * @date 2018/09/05
+ */
 @SpringBootApplication
 public class EvoUnitTestApplication {
 
