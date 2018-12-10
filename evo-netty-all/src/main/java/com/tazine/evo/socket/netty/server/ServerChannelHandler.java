044021d695e93c4903059b87d80e060a1736fcae
Frank <bookfrank@foxmail.com>
2018-12-10 11:00:16 +0800

    feat (netty) ：Netty Channel Override 方法。

--- evo-netty-all/src/main/java/com/tazine/evo/socket/netty/server/ServerChannelHandler.java
+++ evo-netty-all/src/main/java/com/tazine/evo/socket/netty/server/ServerChannelHandler.java
@@ -3,7 +3,11 @@ package com.tazine.evo.socket.netty.server;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import java.net.InetAddress;
+
 /**
+ * ServerChannelHandler
+ *
  * @author frank
  * @date 2018/11/05
  */
@@ -14,7 +18,34 @@ public class ServerChannelHandler extends SimpleChannelInboundHandler<String> {
         // 通信方地址
         System.out.println(ctx.channel().remoteAddress() + " Say : " + msg);
 
-        // 响应客户端消息
+        // 响应客户端消息 必须加 \n 否则可能会出错
         ctx.writeAndFlush(" Received your message,\n ");
+    }
+
+    /**
+     * 当连接建立的时候触发
+     *
+     * @param ctx Context
+     * @throws Exception e
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("RamoteAddress : " + ctx.channel().remoteAddress() + " active !");
+
+        ctx.writeAndFlush("Welcome to " + InetAddress.getLocalHost().getHostName() + " service!\n");
+
+        super.channelActive(ctx);
+    }
+
+    /**
+     * 当连接关闭时触发
+     *
+     * @param ctx ctx
+     * @throws Exception e
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("RamoteAddress : " + ctx.channel().remoteAddress() + " inactive !");
+        super.channelInactive(ctx);
     }
 }
--- evo-netty-all/src/main/java/com/tazine/evo/socket/netty/server/ServerChannelHandlerChain.java
+++ evo-netty-all/src/main/java/com/tazine/evo/socket/netty/server/ServerChannelHandlerChain.java
@@ -1,7 +1,12 @@
 package com.tazine.evo.socket.netty.server;
 
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 
 /**
  * ChannelHandler 职责链
@@ -12,6 +17,16 @@ import io.netty.channel.socket.SocketChannel;
 public class ServerChannelHandlerChain extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
 
+        // 以("\n")为结尾分割的解码器
+        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+
+        // 字符串解码和编码
+        pipeline.addLast("decoder", new StringDecoder());
+        pipeline.addLast("encoder", new StringEncoder());
+
+        // 自己的逻辑 Handler，用于写自己的处理逻辑
+        pipeline.addLast(new ServerChannelHandler());
     }
 }
