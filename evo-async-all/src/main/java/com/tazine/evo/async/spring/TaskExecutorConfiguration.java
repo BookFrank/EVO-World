f78927638d785dee6fc0be4319f19b06e76db93c
Frank <bookfrank@foxmail.com>
2018-12-14 23:54:21 +0800

    feat (async) : 异步可视化线程池测试。

--- evo-async-all/src/main/java/com/tazine/evo/async/spring/TaskExecutorConfiguration.java
+++ evo-async-all/src/main/java/com/tazine/evo/async/spring/TaskExecutorConfiguration.java
@@ -1,5 +1,7 @@
 package com.tazine.evo.async.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -17,6 +19,8 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 @Configuration
 public class TaskExecutorConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskExecutorConfiguration.class);
 
     /**
      * 核心线程数
@@ -50,6 +54,28 @@ public class TaskExecutorConfiguration {
         // AbortPolicy：对拒绝任务抛弃处理，并且抛出异常。
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
 
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean(name = "async-service")
+    public Executor asyncServiceExecutor() {
+        logger.info("start asyncServiceExecutor");
+        //使用VisiableThreadPoolTaskExecutor
+        ThreadPoolTaskExecutor executor = new VisiableThreadPoolExecutor();
+        //配置核心线程数
+        executor.setCorePoolSize(5);
+        //配置最大线程数
+        executor.setMaxPoolSize(5);
+        //配置队列大小
+        executor.setQueueCapacity(99999);
+        //配置线程池中的线程的名称前缀
+        executor.setThreadNamePrefix("async-service-");
+
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //执行初始化
         executor.initialize();
         return executor;
     }
