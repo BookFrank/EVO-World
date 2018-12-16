3f23bd4aad7276e483bf9143f3e0517ceaef48c6
Frank <bookfrank@foxmail.com>
2018-12-16 21:16:43 +0800

    feat (async) : 增加 Future 异步实现学习。

---
+++ evo-async-all/src/main/java/com/tazine/evo/async/method/future/FutureDemo.java
@@ -0,0 +1,16 @@
+package com.tazine.evo.async.method.future;
+
+/**
+ * FutureDemo
+ *
+ * @author frank
+ * @date 2018/12/16
+ */
+public class FutureDemo {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
