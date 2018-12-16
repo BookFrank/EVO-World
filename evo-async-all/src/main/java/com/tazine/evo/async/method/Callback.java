7c9b1a3a4adc0742ad8c78e0a1d331f084b1afa5
Frank <bookfrank@foxmail.com>
2018-12-16 20:40:23 +0800

    feat (async) : 增加 callback 实践。

---
+++ evo-async-all/src/main/java/com/tazine/evo/async/method/Callback.java
@@ -0,0 +1,8 @@
+package com.tazine.evo.async.method;
+
+/**
+ * @author frank
+ * @date 2018/12/15
+ */
+public class Callback {
+}
---
+++ evo-async-all/src/main/java/com/tazine/evo/async/method/callback/Worker.java
@@ -0,0 +1,11 @@
+package com.tazine.evo.async.method.callback;
+
+/**
+ * Worker
+ * @author frank
+ * @date 2018/12/16
+ */
+public class Worker {
+
+    
+}
