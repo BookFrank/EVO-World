1775dad0edb469dcd17da1aa30f1a42113a3b5bc
Frank <bookfrank@foxmail.com>
2018-12-31 16:52:15 +0800

    feat (vue) : 增加前端 vue 工程代码。

---
+++ evo-springboot-vue/src/main/resources/vue/src/components/EvoLogin.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <page-header></page-header>
+        <hr/>
+    </div>
+</template>
+
+<script>
+import pageHeader from '@/components/PageHeader.vue'
+
+export default {
+    
+}
+</script>
---
+++ evo-springboot-vue/src/main/resources/vue/src/components/PageHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    头部信息
+  </div>
+</template>
+
+
+<script>
+  export default{
+    name: "PageHeader"
+  }
+</script>
