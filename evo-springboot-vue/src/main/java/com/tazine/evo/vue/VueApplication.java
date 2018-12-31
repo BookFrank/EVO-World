a7bc7ea0b3ef08e379004793d64b2901b4ade086
Frank <bookfrank@foxmail.com>
2018-12-31 11:03:22 +0800

    feat (vue) : vue 模块依赖梳理。

---
+++ evo-springboot-vue/src/main/java/com/tazine/evo/vue/VueApplication.java
@@ -0,0 +1,18 @@
+package com.tazine.evo.vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * VueApplication
+ *
+ * @author frank
+ * @date 2018-12-31
+ */
+@SpringBootApplication
+public class VueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueApplication.class, args);
+    }
+}
--- evo-springboot-vue/src/test/java/com/tazine/evo/vue/VueApplicationTests.java
+++ evo-springboot-vue/src/test/java/com/tazine/evo/vue/VueApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EvoSpringbootVueApplicationTests {
+public class VueApplicationTests {
 
 	@Test
 	public void contextLoads() {
