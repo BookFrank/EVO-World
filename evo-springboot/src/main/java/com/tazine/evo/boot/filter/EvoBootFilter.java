afde4489af69417dd577f02b0c4206450adb6e41
Frank <bookfrank@foxmail.com>
2018-12-31 21:19:19 +0800

    feat (springboot) : 优化代码POM结构。

--- evo-springboot/src/main/java/com/tazine/evo/boot/filter/EvoBootFilter.java
+++ evo-springboot/src/main/java/com/tazine/evo/boot/filter/EvoBootFilter.java
@@ -13,10 +13,20 @@ import java.io.IOException;
 @WebFilter(urlPatterns = "/*")
 public class EvoBootFilter implements Filter {
     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
         throws IOException, ServletException {
         System.err.println("进入 EvoBootFilter");
         filterChain.doFilter(servletRequest,servletResponse);
         System.err.println("退出 EvoBootFilter");
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
