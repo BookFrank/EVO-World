1a28e8d79233366e13d1d22e46a18e5f671b62cb
Frank <bookfrank@foxmail.com>
2019-01-03 20:53:24 +0800

    feat (infrastucture) : 基础设施包POM。

---
+++ evo-infrastructure/src/main/java/com/tazine/evo/infrastructure/Application.java
@@ -0,0 +1,18 @@
+package com.tazine.evo.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 基础设施
+ *
+ * @author frank
+ * @date 2018/11/11
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
