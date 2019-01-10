dec03ba7ace9c77b740a59bd80f6d9ffddfcf13d
Frank <bookfrank@foxmail.com>
2019-01-10 23:58:11 +0800

    feat (ddd) : 领域驱动 POM 整理。

---
+++ evo-ddd/src/main/java/com/tazine/evo/ddd/DDDApplication.java
@@ -0,0 +1,19 @@
+package com.tazine.evo.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 领域驱动学习实践
+ *
+ * @author frank
+ * @date 2019/1/10
+ */
+//@ServletComponentScan
+@SpringBootApplication
+public class DDDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DDDApplication.class, args);
+    }
+}
