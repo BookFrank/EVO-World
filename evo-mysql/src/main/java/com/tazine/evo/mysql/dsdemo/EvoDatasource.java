a9e7a2c85fd7a66eecf2c8ce4ec058f3e483f8f3
Frank <bookfrank@foxmail.com>
2019-01-13 19:13:26 +0800

    feat (mysql) : 自定义datasource。

---
+++ evo-mysql/src/main/java/com/tazine/evo/mysql/dsdemo/EvoDatasource.java
@@ -0,0 +1,88 @@
+package com.tazine.evo.mysql.dsdemo;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.LinkedList;
+import java.util.logging.Logger;
+
+/**
+ * 自定义 DataSource
+ *
+ * @author frank
+ * @date 2019/1/13
+ */
+public class EvoDatasource implements DataSource {
+
+    private static final LinkedList<Connection> pool = new LinkedList<>();
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 创建数据源时建立固定数量的数据库连接
+     */
+    public EvoDatasource() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root", "");
+                pool.add(conn);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return pool.removeFirst();
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+}
