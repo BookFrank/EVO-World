e60dd2967642d36812c53721a4abfa14fd5b583b
Frank <bookfrank@foxmail.com>
2019-01-16 20:03:10 +0800

    test (test) : 修改启动类。

---
+++ evo-test-only/src/main/java/com/tazine/evo/test/TestOnlyApplication.java
@@ -0,0 +1,18 @@
+package com.tazine.evo.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TestOnlyApplication
+ *
+ * @author frank
+ * @date 2018/12/22
+ */
+@SpringBootApplication
+public class TestOnlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestOnlyApplication.class, args);
+    }
+}
