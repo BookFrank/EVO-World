0293767e769204df6e1e9417ca437616626758d9
Frank <bookfrank@foxmail.com>
2019-01-29 15:40:21 +0800

    feat (neo4j) : 应用搭建。

--- evo-neo4j/src/main/java/com/tazine/evo/neo4j/Neo4jApplication.java
+++ evo-neo4j/src/main/java/com/tazine/evo/neo4j/Neo4jApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EvoNeo4jApplication {
+public class Neo4jApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EvoNeo4jApplication.class, args);
+		SpringApplication.run(Neo4jApplication.class, args);
 	}
 }
