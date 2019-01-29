7c0f46251564dc45bcc5a7c49d3ec2510ce66636
Frank <bookfrank@foxmail.com>
2019-01-29 17:46:12 +0800

    feat (neo4j) : 整理 neo4j 工程。

--- evo-neo4j/src/main/java/com/tazine/evo/neo4j/Neo4jApplication.java
+++ evo-neo4j/src/main/java/com/tazine/evo/neo4j/Neo4jApplication.java
@@ -1,7 +1,7 @@
 package com.tazine.evo.neo4j;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Neo4jApplication {
