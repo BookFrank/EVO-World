040f3ef7a4fcb59cb0aecc0d9fae129b3de8e6ac
Frank <bookfrank@foxmail.com>
2019-02-01 11:56:27 +0800

    feat (neo4j) : 重建 neo4j 模块。

---
+++ evo-neo4j-d3/src/main/java/com/tazine/evo/evo4j/Neo4jApplication.java
@@ -0,0 +1,18 @@
+package com.tazine.evo.evo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Neo4jApplication
+ *
+ * @author frank
+ * @date 2018/11/11
+ */
+@SpringBootApplication
+public class Neo4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jApplication.class, args);
+    }
+}
