279bef043a4b317f146e8a4721d715dc014aacfd
Frank <bookfrank@foxmail.com>
2019-03-01 18:31:30 +0800

    feat (springboot) : 增加 springboot-2x 模块。

---
+++ evo-springboot-2x/src/main/java/com/tazine/evo/boot/Application.java
@@ -0,0 +1,20 @@
+package com.tazine.evo.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * SpringBoot2.0 实践
+ *
+ * @author frank
+ * @date 2018/11/11
+ */
+@ServletComponentScan
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
