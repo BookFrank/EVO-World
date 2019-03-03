4110a709628f6aa674e901a76f2c5a3fd082a475
Frank <bookfrank@foxmail.com>
2019-03-03 23:59:33 +0800

    feat (boot2) : http response.

---
+++ evo-springboot-2x/src/main/java/com/tazine/evo/boot/entity/HttpResult.java
@@ -0,0 +1,55 @@
+package com.tazine.evo.boot.entity;
+
+import com.tazine.evo.boot.entity.enums.HttpAnswerCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * HTTP 响应对象
+ *
+ * @author jiaer.ly
+ * @date 2018/05/02
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class HttpResult<T> {
+
+    private int code;
+    private String msg;
+    private T data;
+
+    public HttpResult(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public static <T> HttpResult newInstance() {
+        return new HttpResult();
+    }
+
+    public HttpResult(HttpAnswerCode answerCode) {
+        this.code = answerCode.getCode();
+        this.msg = answerCode.getInfo();
+    }
+
+    public HttpResult(HttpAnswerCode answerCode, String msg) {
+        this.code = answerCode.getCode();
+        this.msg = msg;
+    }
+
+    public HttpResult(int code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public HttpResult(HttpAnswerCode answerCode, T data) {
+        this.code = answerCode.getCode();
+        this.msg = answerCode.getInfo();
+        this.data = data;
+    }
+}
---
+++ evo-springboot-2x/src/main/java/com/tazine/evo/boot/entity/PageResult.java
@@ -0,0 +1,24 @@
+package com.tazine.evo.boot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * 分页对象
+ *
+ * @author jiaer.ly
+ * @date 2018/05/03
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResult<T> {
+    private Integer currentPage;
+    private Integer totalPage;
+    private List<T> list;
+}
