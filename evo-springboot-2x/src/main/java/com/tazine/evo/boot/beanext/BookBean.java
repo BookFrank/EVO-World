c1fe068be3409ba6cf4fdded9d541027ca1e8e26
Frank <bookfrank@foxmail.com>
2019-03-04 00:00:13 +0800

    feat (boo2) : bean ext.

---
+++ evo-springboot-2x/src/main/java/com/tazine/evo/boot/beanext/BookBean.java
@@ -0,0 +1,26 @@
+package com.tazine.evo.boot.beanext;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 实现了 InitializingBean 接口的 Bean 在实例化时会先调用 afterPropertiesSet 方法
+ *
+ * @author frank
+ * @date 2018/10/31
+ */
+@Component
+public class BookBean implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.err.println("BookBean 正在进行初始化");
+    }
+
+    @PostConstruct
+    public void init(){
+        System.err.println("BookBean PostConstruct 执行");
+    }
+}
---
+++ evo-springboot-2x/src/main/java/com/tazine/evo/boot/beanext/FactoryBeanTest.java
@@ -0,0 +1,27 @@
+package com.tazine.evo.boot.beanext;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * 使用 @Component + FactoryBean接口 创建 Bean
+ *
+ * @author frank
+ * @date 2018/10/31
+ */
+@Component
+public class FactoryBeanTest implements FactoryBean<NioCar> {
+
+    @Override
+    public NioCar getObject() throws Exception {
+        // 可以通过此方式将第三方 Jar 包中的类创建为一个 Bean 来进行管理
+        // 并可在创建 Bean 之前对 bean 进行一系列的配置工作
+        // e.g. ES TransportClient
+        return new NioCar();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return NioCar.class;
+    }
+}
