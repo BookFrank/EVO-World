699b4b0efdd437e8d00249257b851cf51d67c5f6
Frank <bookfrank@foxmail.com>
2019-03-04 10:55:31 +0800

    feat (boot2) : retry 策略学习。

---
+++ evo-springboot-2x/src/main/java/com/tazine/evo/boot/retry/TryCatchRetry.java
@@ -0,0 +1,38 @@
+package com.tazine.evo.boot.retry;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * TryCatchRedo-简单重试模式
+ *
+ * @author jiaer.ly
+ * @date 2019/03/04
+ */
+public class TryCatchRetry {
+
+    /**
+     * try-catch-redo 重试模式有可能出现重试无效，解决办法是尝试增加重试次数 retrycount 以及重试间隔周期 interval， 达到增加重试有效的可能性。
+     *
+     * @param args args
+     */
+    public static void main(String[] args) {
+        String result;
+        try {
+            result = getResult();
+
+            // 一次重试
+            if (StringUtils.isEmpty(result)) {
+                result = getResult();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 又一次重试
+            result = getResult();
+        }
+        System.out.println(result);
+    }
+
+    private static String getResult() {
+        return "Hello World";
+    }
+}
---
+++ evo-springboot-2x/src/main/java/com/tazine/evo/boot/retry/TryCatchRetryWithStrategy.java
@@ -0,0 +1,44 @@
+package com.tazine.evo.boot.retry;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * 策略重试模式
+ *
+ * @author jiaer.ly
+ * @date 2019/03/04
+ */
+public class TryCatchRetryWithStrategy {
+
+    public static void main(String[] args) throws InterruptedException {
+        String result;
+        try {
+            result = getResult();
+
+            // 延迟3s，重试3次
+            if (StringUtils.isEmpty(result)) {
+                result = retryGetResult(3000L,3);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 又一次重试
+            result = retryGetResult(3000L,3);
+        }
+        System.out.println(result);
+    }
+
+    private static String getResult() {
+        return "Hello World";
+    }
+
+    private static String retryGetResult(long delay, int count) throws InterruptedException {
+        Thread.sleep(delay);
+        for (int i = 0; i<count; i++){
+            String ret = getResult();
+            if (!StringUtils.isEmpty(ret)){
+                return ret;
+            }
+        }
+        return null;
+    }
+}
