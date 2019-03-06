202791ffd7373d3d9c84fceeecc050ebebf188f9
Frank <bookfrank@foxmail.com>
2019-03-06 11:29:47 +0800

    feat (async) : listenable 使用。

--- evo-async-all/src/main/java/com/tazine/evo/async/spring/TaskExecutorConfiguration.java
+++ evo-async-all/src/main/java/com/tazine/evo/async/spring/TaskExecutorConfiguration.java
@@ -38,7 +38,7 @@ public class TaskExecutorConfiguration {
     private int queueCapacity = 5;
 
     @Bean(name = "task-pool-1")
-    public Executor asyncExecutor() {
+    public ThreadPoolTaskExecutor asyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(corePoolSize);
         executor.setMaxPoolSize(maxPoolSize);
@@ -59,7 +59,8 @@ public class TaskExecutorConfiguration {
     }
 
     @Bean(name = "async-service")
-    public Executor asyncServiceExecutor() {
+    //public Executor asyncServiceExecutor() {
+    public ThreadPoolTaskExecutor asyncServiceExecutor() {
         logger.info("start asyncServiceExecutor");
         //使用VisiableThreadPoolTaskExecutor
         ThreadPoolTaskExecutor executor = new VisiableThreadPoolExecutor();
@@ -70,7 +71,7 @@ public class TaskExecutorConfiguration {
         //配置队列大小
         executor.setQueueCapacity(99999);
         //配置线程池中的线程的名称前缀
-        executor.setThreadNamePrefix("async-service-");
+        executor.setThreadNamePrefix("visiable-async-");
 
         // rejection-policy：当pool已经达到max size的时候，如何处理新任务
         // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
