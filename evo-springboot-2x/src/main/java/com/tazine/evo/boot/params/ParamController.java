2beb5a0db69ba98db02449e5bfb9c0406ef80384
Frank <bookfrank@foxmail.com>
2019-03-19 16:13:26 +0800

    doc (boot2) : 当参数为数组时，测试。

--- evo-springboot-2x/src/main/java/com/tazine/evo/boot/params/ParamController.java
+++ evo-springboot-2x/src/main/java/com/tazine/evo/boot/params/ParamController.java
@@ -1,6 +1,7 @@
 package com.tazine.evo.boot.params;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.Charsets;
 import com.tazine.evo.boot.params.validation.NbaDraftRequest;
 import org.apache.commons.io.IOUtils;
@@ -38,6 +39,11 @@ public class ParamController {
 
         // req.getInputStream() 中可以拿到头为 text/plain 和 application/json 的请求POST来的数据
         String s = IOUtils.toString(request.getInputStream(), Charsets.UTF_8);
+        JSONObject jsonObject = JSON.parseObject(s);
+        // 当参数为数组时，class 为 JSONArray
+        System.out.println(jsonObject.get("name").getClass().getName());
+        System.out.println(jsonObject.getString("name"));
+
         System.out.println("req.getInputStream():" + s + " , 可以获取到 json");
         return s;
     }
@@ -62,7 +68,6 @@ public class ParamController {
      * @param draftRequest NbaDraftRequest
      * @param names        names
      * @return s
-     * @throws IOException e
      */
     @RequestMapping("/param/body2")
     public List<String> jsonPost(@RequestBody NbaDraftRequest draftRequest, @RequestParam("names") List<String> names) {
@@ -75,12 +80,18 @@ public class ParamController {
      * request.getParameterMap 可以获得URL中的参数和 application/x-www-form-urlencoded 以及 application/form-data 中传递的参数
      *
      * @param request ServletRequest
-     * @return Map<String, String []>
+     * @return Map
      */
     @RequestMapping("/param/map")
     public Map<String, String[]> request(ServletRequest request) {
         //return name + " - " + age + " - " + email;
         // getParameterMap 可以同时获得URL传参和 form-data
+        System.out.println(request.getParameter("name"));
+
+        // 127.0.0.1:7001/param/map?name=kobe&name=james
+        // request.getParameter("name") 只能得到kobe
+        // request.getParameterMap() 可以得到 name:[kobe,james]
+
         return request.getParameterMap();
     }
 }
