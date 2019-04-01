ae791d92585ca8c721bf0c084fbd54664647a071
Frank <bookfrank@foxmail.com>
2019-04-01 22:46:41 +0800

    feat (file) : 传输协议。

---
+++ evo-file-io/src/main/java/com/tazine/evo/file/transfer/BinaryTransfer.java
@@ -0,0 +1,17 @@
+package com.tazine.evo.file.transfer;
+
+/**
+ * Created by lina on 2019/3/31.
+ *
+ * @author frank
+ * @date 2019/3/31
+ */
+public class BinaryTransfer {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ evo-file-io/src/main/java/com/tazine/evo/file/transfer/CharTransfer.java
@@ -0,0 +1,10 @@
+package com.tazine.evo.file.transfer;
+
+/**
+ * Created by lina on 2019/3/31.
+ *
+ * @author frank
+ * @date 2019/3/31
+ */
+public class CharTransfer {
+}
