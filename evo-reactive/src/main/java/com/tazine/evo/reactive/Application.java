e37b156d87c7a4e711e41edfe2c8ece4e3a77cbe
Frank <bookfrank@foxmail.com>
2019-04-04 23:27:11 +0800

    feat (reactive) : 增加 reactive 模块。

---
+++ evo-reactive/src/main/java/com/tazine/evo/reactive/Application.java
@@ -0,0 +1,18 @@
+package com.tazine.evo.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Reactive 实践
+ *
+ * @author frank
+ * @date 2018/11/11
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
