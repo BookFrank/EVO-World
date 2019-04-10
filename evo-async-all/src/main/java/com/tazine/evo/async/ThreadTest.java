69cc6c81d87d9e2e43548776427411e1c43f93f2
Frank <bookfrank@foxmail.com>
2019-04-10 23:14:01 +0800

    feat (async) : daemon thread review。

--- evo-async-all/src/main/java/com/tazine/evo/async/ThreadTest.java
+++ evo-async-all/src/main/java/com/tazine/evo/async/ThreadTest.java
@@ -1,25 +1,26 @@
 package com.tazine.evo.async;
 
 /**
- * @author jiaer.ly
+ * ThreadTest
+ *
+ * @author frank
  * @date 2018/12/21
  */
 public class ThreadTest {
 
     public static void main(String[] args) {
-        Thread.currentThread().setDaemon(true);
-
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     Thread.sleep(10000);
+                    System.out.println("Thread run");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }).start();
         System.out.println("Hello World");
+        Thread.currentThread().setDaemon(true);
     }
-
 }
