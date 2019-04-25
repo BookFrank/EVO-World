32706ae3481027249eb53170d7e7eee5e793ffb8
Frank <bookfrank@foxmail.com>
2019-04-25 23:03:33 +0800

    mod (shiro) : 增加shiro模块。

---
+++ evo-shiro/src/main/java/com/tazine/evo/shiro/App.java
@@ -0,0 +1,13 @@
+package com.tazine.evo.shiro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
