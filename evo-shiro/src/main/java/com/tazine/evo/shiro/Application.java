77ae49d79085d9a26a7b5687883f06d046ad16c8
Frank <bookfrank@foxmail.com>
2019-04-26 23:09:32 +0800

    feat (shiro) : boot 配置。

---
+++ evo-shiro/src/main/java/com/tazine/evo/shiro/Application.java
@@ -0,0 +1,18 @@
+package com.tazine.evo.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot Shiro 框架实践
+ *
+ * @author frank
+ * @date 2018/11/11
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
