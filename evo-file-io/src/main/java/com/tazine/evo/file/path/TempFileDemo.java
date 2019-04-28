8a6cf1c08806eab049844055c3c85edafb6bcbdc
Frank <bookfrank@foxmail.com>
2019-04-28 23:48:39 +0800

    feat (file) : Files temp file practice.

---
+++ evo-file-io/src/main/java/com/tazine/evo/file/path/TempFileDemo.java
@@ -0,0 +1,16 @@
+package com.tazine.evo.file.path;
+
+import java.nio.file.Files;
+
+/**
+ * TempFileDemo
+ *
+ * @author frank
+ * @date 2019-04-28
+ */
+public class TempFileDemo {
+
+    public static void main(String[] args) {
+//        Files.createTempFile();
+    }
+}
