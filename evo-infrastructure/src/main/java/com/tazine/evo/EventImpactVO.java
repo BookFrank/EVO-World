66a2aa9669b53fcf154ff46c99cbb8aa6ff08e24
Frank <bookfrank@foxmail.com>
2019-04-30 22:50:41 +0800

    feat (infra) : JSON 转整型Map问题

---
+++ evo-infrastructure/src/main/java/com/tazine/evo/EventImpactVO.java
@@ -0,0 +1,23 @@
+package com.tazine.evo;
+
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * 事件影响结果对象
+ *
+ * @author frank
+ * @date 2019/04/25
+ */
+@Data
+public class EventImpactVO {
+
+    private String sEventId;
+
+    private String source;
+
+    private int total;
+
+    private Map<Integer, Integer> detail;
+}
---
+++ evo-infrastructure/src/main/java/com/tazine/evo/TestController.java
@@ -0,0 +1,30 @@
+package com.tazine.evo;
+
+import com.tazine.evo.common.RestResponseBuilder;
+import com.tazine.evo.common.entity.HttpResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author frank
+ * @date 2019/04/26
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public HttpResult test(){
+        EventImpactVO vo = new EventImpactVO();
+
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(20190425, 31);
+        vo.setDetail(map);
+        vo.setSEventId("123");
+        vo.setTotal(31);
+        vo.setSource("event_guangzhou");
+        return RestResponseBuilder.buildSuccessResponse(vo);
+    }
+}
