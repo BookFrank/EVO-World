7eab162a2f0ad553769ba4dbdb627c9593a12852
Frank <bookfrank@foxmail.com>
2019-05-27 10:38:48 +0800

    feat (boot1) : red lock getset test.

--- evo-springboot-1x/src/main/java/com/tazine/evo/boot/util/redlock/ProdRedLock.java
+++ evo-springboot-1x/src/main/java/com/tazine/evo/boot/util/redlock/ProdRedLock.java
@@ -3,6 +3,7 @@ package com.tazine.evo.boot.util.redlock;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 /**
@@ -12,9 +13,8 @@ import org.springframework.util.StringUtils;
  * @date 2019/05/26
  */
 @Slf4j
+@Component
 public class ProdRedLock {
-
-    private static final int TIMEOUT = 120 * 1000;
 
     @Autowired
     private RedisTemplate redisTemplate;
@@ -26,15 +26,15 @@ public class ProdRedLock {
         //currentValue=A   这两个线程的value都是B  其中一个线程拿到锁
         String currentValue = (String)redisTemplate.opsForValue().get(lockKey);
         //如果锁过期
-        if (!StringUtils.isEmpty(currentValue)
-            && Long.parseLong(currentValue) < System.currentTimeMillis()) {
+        if (!StringUtils.isEmpty(currentValue) && Long.parseLong(currentValue) < System.currentTimeMillis()) {
+            log.info("[PROD-RED-LOCK] lock timeout");
             //获取上一个锁的时间
             String oldValue = (String)redisTemplate.opsForValue().getAndSet(lockKey, lockVal);
             if (!StringUtils.isEmpty(oldValue) && oldValue.equals(currentValue)) {
+                log.info("[PROD-RED-LOCK] {} lock success", lockKey);
                 return true;
             }
         }
-
         return false;
     }
 
@@ -43,6 +43,7 @@ public class ProdRedLock {
             String currentValue = (String)redisTemplate.opsForValue().get(lockKey);
             if (!StringUtils.isEmpty(currentValue) && currentValue.equals(lockVal)) {
                 redisTemplate.opsForValue().getOperations().delete(lockKey);
+                log.info("[PROD-RED-LOCK] unlock success");
             }
         }catch (Exception e) {
             log.error("【redis分布式锁】解锁异常, {}", e);
--- evo-springboot-1x/src/test/java/com/tazine/evo/boot/RedisLockTest.java
+++ evo-springboot-1x/src/test/java/com/tazine/evo/boot/RedisLockTest.java
@@ -1,5 +1,6 @@
 package com.tazine.evo.boot;
 
+import com.tazine.evo.boot.util.redlock.ProdRedLock;
 import com.tazine.evo.boot.util.redlock.SimpRedLock;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -22,15 +23,16 @@ public class RedisLockTest {
     @Autowired
     private SimpRedLock simpRedLock;
 
-    @Test
-    public void testLockOne() {
-        String prod = "IPhoneXI";
+    @Autowired
+    private ProdRedLock prodRedLock;
 
-        new Thread(() -> processSecKill(prod)).start();
-        new Thread(() -> processSecKill(prod)).start();
-        new Thread(() -> processSecKill(prod)).start();
-        new Thread(() -> processSecKill(prod)).start();
-        new Thread(() -> processSecKill(prod)).start();
+    @Test
+    public void testLockProd() {
+        new Thread(this::prodLockTest).start();
+        new Thread(this::prodLockTest).start();
+        new Thread(this::prodLockTest).start();
+        new Thread(this::prodLockTest).start();
+        new Thread(this::prodLockTest).start();
 
         try {
             Thread.sleep(20000);
@@ -40,7 +42,49 @@ public class RedisLockTest {
         System.err.println("main 线程执行完成");
     }
 
-    private void processSecKill(String product) {
+    @Test
+    public void testLockOne() {
+        String prod = "IPhoneXI";
+
+        new Thread(() -> simpLockTest(prod)).start();
+        new Thread(() -> simpLockTest(prod)).start();
+        new Thread(() -> simpLockTest(prod)).start();
+        new Thread(() -> simpLockTest(prod)).start();
+        new Thread(() -> simpLockTest(prod)).start();
+
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.err.println("main 线程执行完成");
+    }
+
+    private void prodLockTest(){
+        String lockKey = "lock.watch";
+        String lockVal = String.valueOf(System.currentTimeMillis() + 5 * 1000);
+
+        String threadName = Thread.currentThread().getName();
+        // 加锁
+        while (!prodRedLock.lock(lockKey, lockVal)) {
+            try {
+                Thread.sleep(500);
+                System.err.println("   " + threadName + " 尝试获取锁");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.err.println(threadName + " 获得锁");
+        System.out.println("  " + threadName + " 执行业务逻辑...");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        prodRedLock.unlock(lockKey, lockVal);
+    }
+
+    private void simpLockTest(String product) {
         String lockKey = "lock." + product;
         String lockVal = Thread.currentThread().getName() + "-" + product;
 
