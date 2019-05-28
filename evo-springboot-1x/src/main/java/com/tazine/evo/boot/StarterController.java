ce8289f1d22726c4453b1196e324e30d3e35ec5f
Frank <bookfrank@foxmail.com>
2019-05-28 21:18:56 +0800

    feat (boot) : starter test.

---
+++ evo-springboot-1x/src/main/java/com/tazine/evo/boot/StarterController.java
@@ -0,0 +1,23 @@
+package com.tazine.evo.boot;
+
+//import com.tazine.evo.infrastructure.TestBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author frank
+ * @date 2019/05/28
+ */
+@RestController
+public class StarterController {
+
+    //@Autowired
+    //private TestBean testBean;
+    //
+    //@RequestMapping("/h")
+    //public String hi(){
+    //    testBean.hi();
+    //    return "hi";
+    //}
+}
