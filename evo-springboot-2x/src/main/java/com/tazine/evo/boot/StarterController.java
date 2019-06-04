214d3405b56f05cb135e14887a4aa6f2a9994b88
Frank <bookfrank@foxmail.com>
2019-06-04 18:24:26 +0800

    feat (boot2) : import test.

--- evo-springboot-2x/src/main/java/com/tazine/evo/boot/StarterController.java
+++ evo-springboot-2x/src/main/java/com/tazine/evo/boot/StarterController.java
@@ -1,6 +1,6 @@
 package com.tazine.evo.boot;
 
-import com.tazine.evo.infrastructure.TestBean;
+import com.tazine.evo.infrastructure.starter.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class StarterController {
 
     @Autowired
-    private TestBean testBean;
+    private TestService testService;
 
     @RequestMapping("/h")
     public String hi(){
-        testBean.hi();
+        testService.hi();
         return "hi";
     }
 }
---
+++ evo-springboot-2x/src/main/java/com/tazine/evo/boot/aware/ContextProvider.java
@@ -0,0 +1,27 @@
+package com.tazine.evo.boot.aware;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * ApplicationContextProvider
+ *
+ * @author jiaer.ly
+ * @date 2018/12/18
+ */
+@Component
+public class ContextProvider implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        applicationContext = context;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+}
