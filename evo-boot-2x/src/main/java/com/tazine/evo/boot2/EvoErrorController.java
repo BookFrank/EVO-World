0a725e6e4e20548cdcf93c46751b77b5686c0423
Frank <bookfrank@foxmail.com>
2019-10-22 19:47:46 +0800

    feat (boot2) : another way.

--- evo-boot-2x/src/main/java/com/tazine/evo/boot2/EvoErrorController.java
+++ evo-boot-2x/src/main/java/com/tazine/evo/boot2/EvoErrorController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ import java.util.Map;
  * @author frank
  * @date 2019/10/21
  */
-//@RestController
+@RestController
 public class EvoErrorController extends AbstractErrorController {
 
     private final static String ERROR_PATH = "/error";
@@ -42,29 +43,87 @@ public class EvoErrorController extends AbstractErrorController {
     //}
 
     // 实现二
+    //@RequestMapping(value = ERROR_PATH)
+    //public JSONObject error(HttpServletRequest request, HttpServletRequest response) {
+    //    // 1. 校验输入参数中是否有 debug 参数
+    //    boolean debug = StringUtils.equalsIgnoreCase("true", request.getParameter("debug"));
+    //
+    //    // 2. 通过 debug=true 来判断是否需要输出堆栈错误信息
+    //    Map<String, Object> body = getErrorAttributes(request, debug);
+    //
+    //    // 3. 组装成业务自定义的通用响应
+    //    int status = (int)body.get("status");  // 获取标准 HTTP 响应状态
+    //    String message = (String)body.get("message"); // 程序执行过程中的错误信息
+    //    Throwable cause = getCause(request); // 获取异常，有可能为空
+    //    String errorMessage; // 友好提示信息
+    //    if (null != cause) {
+    //        errorMessage = getErrorMessage(cause);
+    //    } else {
+    //        errorMessage = (String)body.get("error");
+    //    }
+    //
+    //    JSONObject jsonObject = new JSONObject();
+    //    jsonObject.put("code", status);
+    //    jsonObject.put("msg", errorMessage);
+    //    jsonObject.put("data", message);
+    //    return jsonObject;
+    //}
+
+    // 实现三
+    //@RequestMapping(value = ERROR_PATH)
+    //public JSONObject error(HttpServletRequest request, HttpServletRequest response) {
+    //    Map<String, Object> body = getErrorAttributes(request, true);
+    //    System.out.println();
+    //
+    //    // 3. 组装成业务自定义的通用响应
+    //    int status = (int)body.get("status");  // 获取标准 HTTP 响应状态
+    //    String message = (String)body.get("message"); // 程序执行过程中的错误信息
+    //    Throwable cause = getCause(request); // 获取异常，有可能为空
+    //    String errorMessage; // 友好提示信息
+    //    if (null != cause) {
+    //        errorMessage = getErrorMessage(cause);
+    //    } else {
+    //        errorMessage = (String)body.get("error");
+    //    }
+    //    String trace = (String)body.get("trace");
+    //
+    //    System.err.println(trace);
+    //    // 1. 校验输入参数中是否有 debug 参数
+    //    // 2. 通过 debug=true 来判断是否需要输出堆栈错误信息
+    //    boolean debug = StringUtils.equalsIgnoreCase("true", request.getParameter("debug"));
+    //
+    //    JSONObject jsonObject = new JSONObject();
+    //    jsonObject.put("code", status);
+    //    jsonObject.put("msg", errorMessage);
+    //    if (debug){
+    //        jsonObject.put("data", trace);
+    //    }
+    //    return jsonObject;
+    //}
+
+    // 实现四
     @RequestMapping(value = ERROR_PATH)
     public JSONObject error(HttpServletRequest request, HttpServletRequest response) {
-        // 1. 校验输入参数中是否有 debug 参数
-        boolean debug = StringUtils.equalsIgnoreCase("true", request.getParameter("debug"));
-
-        // 2. 通过 debug=true 来判断是否需要输出堆栈错误信息
-        Map<String, Object> body = getErrorAttributes(request, debug);
+        Map<String, Object> body = getErrorAttributes(request, true);
 
         // 3. 组装成业务自定义的通用响应
         int status = (int)body.get("status");  // 获取标准 HTTP 响应状态
         String message = (String)body.get("message"); // 程序执行过程中的错误信息
-        Throwable cause = getCause(request); // 获取异常，有可能为空
-        String errorMessage; // 友好提示信息
-        if (null != cause) {
-            errorMessage = getErrorMessage(cause);
-        } else {
-            errorMessage = (String)body.get("error");
-        }
+        String errorMessage = (String)body.get("error");
+        String trace = (String)body.get("trace");
+        System.err.println(trace);
+
+        // 1. 校验输入参数中是否有 debug 参数
+        // 2. 通过 debug=true 来判断是否需要输出堆栈错误信息
+        boolean debug = StringUtils.equalsIgnoreCase("true", request.getParameter("debug"));
 
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("code", status);
         jsonObject.put("msg", errorMessage);
         jsonObject.put("data", message);
+        if (debug){
+            jsonObject.put("trace", trace);
+        }
         return jsonObject;
     }
 
@@ -83,6 +142,7 @@ public class EvoErrorController extends AbstractErrorController {
      */
     protected Throwable getCause(HttpServletRequest request) {
         Throwable error = (Throwable)request.getAttribute("javax.servlet.error.exception");
+        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
         if (null != error) {
             // SpringMvc 的异常可能会封装成 ServletException，需要调用 getCause 才能获取真正的异常
             while (error instanceof ServletException && null != error.getCause()) {
