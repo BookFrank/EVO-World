34efd6f8664fc5b8d57098b1553b7be7cad85d38
Frank <bookfrank@foxmail.com>
2019-10-22 14:16:48 +0800

    feat (boot2) : error contoller.

--- evo-boot-2x/src/main/java/com/tazine/evo/boot2/EvoErrorController.java
+++ evo-boot-2x/src/main/java/com/tazine/evo/boot2/EvoErrorController.java
@@ -1,9 +1,13 @@
 package com.tazine.evo.boot2;
 
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
@@ -11,20 +15,80 @@ import java.util.Map;
  * @author frank
  * @date 2019/10/21
  */
-//@RestController
+@RestController
 public class EvoErrorController extends AbstractErrorController {
+
+    private final static String ERROR_PATH = "/error";
+
+    private ErrorAttributes errorAttributes;
 
     public EvoErrorController(ErrorAttributes errorAttributes) {
         super(errorAttributes);
+        this.errorAttributes = errorAttributes;
     }
 
-    @Override
-    protected Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
-        return super.getErrorAttributes(request, includeStackTrace);
-    }
-
-    @Override
     public String getErrorPath() {
-        return null;
+        return ERROR_PATH;
+    }
+
+    // 实现一
+    //@RequestMapping(value = ERROR_PATH)
+    //public Map<String, Object> error(HttpServletRequest request, HttpServletRequest response) {
+    //    // 1. 校验输入参数中是否有 debug 参数
+    //    boolean debug = StringUtils.equalsIgnoreCase("true", request.getParameter("debug"));
+    //    // 2. 通过 debug=true 来判断是否需要输出堆栈错误信息
+    //    Map<String, Object> body = getErrorAttributes(request, debug);
+    //    return body;
+    //}
+
+    // 实现二
+    @RequestMapping(value = ERROR_PATH)
+    public JSONObject error(HttpServletRequest request, HttpServletRequest response) {
+        // 1. 校验输入参数中是否有 debug 参数
+        boolean debug = StringUtils.equalsIgnoreCase("true", request.getParameter("debug"));
+
+        // 2. 通过 debug=true 来判断是否需要输出堆栈错误信息
+        Map<String, Object> body = getErrorAttributes(request, debug);
+
+        // 3. 组装成业务自定义的通用响应
+        int status = (int)body.get("status");  // 获取标准 HTTP 响应状态
+        String message = (String)body.get("message"); // 程序执行过程中的错误信息
+        Throwable cause = getCause(request); // 获取异常，有可能为空
+        String errorMessage; // 友好提示信息
+        if (null != cause) {
+            errorMessage = getErrorMessage(cause);
+        } else {
+            errorMessage = (String)body.get("error");
+        }
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", status);
+        jsonObject.put("msg", errorMessage);
+        jsonObject.put("data", message);
+        return jsonObject;
+    }
+
+    protected String getErrorMessage(Throwable throwable) {
+        if (throwable instanceof EvoException) {
+            // 如果 自定义异常信息可以显示给用户
+            return ((EvoException)throwable).getMessage();
+        }
+        return throwable.getMessage();
+    }
+
+    /**
+     * 获取应用系统的异常
+     *
+     * @return Throwable
+     */
+    protected Throwable getCause(HttpServletRequest request) {
+        Throwable error = (Throwable)request.getAttribute("javax.servlet.error.exception");
+        if (null != error) {
+            // SpringMvc 的异常可能会封装成 ServletException，需要调用 getCause 才能获取真正的异常
+            while (error instanceof ServletException && null != error.getCause()) {
+                error = ((ServletException)error).getCause();
+            }
+        }
+        return error;
     }
 }
---
+++ evo-boot-2x/src/main/java/com/tazine/evo/boot2/EvoException.java
@@ -0,0 +1,8 @@
+package com.tazine.evo.boot2;
+
+/**
+ * @author jiaer.ly
+ * @date 2019/10/22
+ */
+public class EvoException extends Throwable{
+}
