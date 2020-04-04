6705501cd5470d9605fd65f2fd5b3955a7f4e4df
Frank <bookfrank@foxmail.com>
2020-04-04 23:23:44 +0800

    feat (async) : 2 ways to realize the async.

--- evo-async/src/main/java/com/tazine/evo/async/jvm/Test.java
+++ evo-async/src/main/java/com/tazine/evo/async/jvm/Test.java
@@ -1,5 +1,6 @@
 package com.tazine.evo.async.jvm;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 /**
@@ -11,9 +12,28 @@ import java.util.concurrent.Future;
 public class Test {
 
     public static void main(String[] args) throws Exception {
-        //syncNonBlocking();
+        // syncBlocking();
 
-        asyncNonBlocking();
+        // syncNonBlocking();
+
+        asyncBlocking();
+
+        // asyncNonBlocking();
+    }
+
+    /**
+     * 在拿到 exec() 方法前，main 线程是阻塞的
+     *
+     * @throws ExecutionException   e1
+     * @throws InterruptedException e2
+     */
+    private static void asyncBlocking() throws ExecutionException, InterruptedException {
+        AsyncBlocking asyncBlocking = new AsyncBlocking();
+
+        Future<String> future = asyncBlocking.exec();
+
+        System.out.println(future.get());
+
     }
 
     private static void asyncNonBlocking() throws InterruptedException {
@@ -24,25 +44,30 @@ public class Test {
         System.out.println("hi");
     }
 
-    public static void callback(String ret){
+    public static void callback(String ret) {
         System.out.println(ret);
     }
 
+    /**
+     * 在拿到 exec() 结果的过程中，main 线程是非阻塞的
+     *
+     * @throws Exception e
+     */
     private static void syncNonBlocking() throws Exception {
         SyncNonBlocking syncNonBlocking = new SyncNonBlocking();
         Future<String> future = syncNonBlocking.exec();
-        while (true){
-            if (future.isDone()){
+        while (true) {
+            if (future.isDone()) {
                 System.out.println(future.get());
                 break;
-            }else {
+            } else {
                 System.out.println("wait");
                 Thread.sleep(1000 * 1);
             }
         }
     }
 
-    private static void syncBlocking(){
+    private static void syncBlocking() {
         SyncBlocking syncBlocking = new SyncBlocking();
         String ret = syncBlocking.exec();
         System.out.println(ret);
