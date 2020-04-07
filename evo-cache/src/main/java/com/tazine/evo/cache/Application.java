9e06c762e5d096936560fbdd74fa6ed10f962576
Frank <bookfrank@foxmail.com>
2020-04-07 20:54:14 +0800

    feat (cache) : add cache module.

---
+++ evo-cache/src/main/java/com/tazine/evo/cache/Application.java
@@ -0,0 +1,18 @@
+package com.tazine.evo.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Cache
+ *
+ * @author frank
+ * @date 2019/1/10
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
